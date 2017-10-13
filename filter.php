2cbdd14e5d5f06414958471f51a1dd9755282171
Ashley Sheridan <ashley.sheridan@livedrive.com>
2017-10-13 10:17:02 +0100

    added oil impressionist filter and refactored

--- filter.php
+++ filter.php
@@ -8,7 +8,7 @@ class filter
 
 	public function __construct($img_path)
 	{
-		$this->load_image($img_path);
+		$this->photo = $this->load_image($img_path);
 	}
 
 	public function apply_filter($filter)
@@ -23,25 +23,12 @@ class filter
 		else
 			echo "filter $filter does not exist";
 	}
-	
-	private function load_image($img_path)
-	{
-		if(file_exists($img_path))
-		{
-			$this->photo = imagecreatefromjpeg($img_path);
-		}
-		else
-		{
-			echo "file $img_path doesn't exist";
-		}
-	}
-	
+
 	private function pumpkins()
 	{
-		$base = imagecreatetruecolor($this::WIDTH, $this::HEIGHT);
-		$pumpkins = imagecreatefrompng("filter_layers/pumpkins.png");
+		$base = $this->photo;
+		$pumpkins = $this->load_image("filter_layers/pumpkins.png");
 		
-		imagecopyresampled($base, $this->photo, 0, 0, 0, 0, $this::WIDTH, $this::HEIGHT, $this::WIDTH, $this::HEIGHT);
 		imagecopyresampled($base, $pumpkins, 0, 0, 0, 0, $this::WIDTH, $this::HEIGHT, $this::WIDTH, $this::HEIGHT);
 
 		return $base;
@@ -49,7 +36,7 @@ class filter
 	
 	private function negate()
 	{
-		$base = imagecreatetruecolor($this::WIDTH, $this::HEIGHT);
+		$base = $this->get_blank_base();
 		
 		imagecopyresampled($base, $this->photo, 0, 0, 0, 0, $this::WIDTH, $this::HEIGHT, $this::WIDTH, $this::HEIGHT);
 		imagefilter($base, IMG_FILTER_NEGATE);
@@ -59,8 +46,8 @@ class filter
 	
 	private function tv()
 	{
-		$base = imagecreatetruecolor($this::WIDTH, $this::HEIGHT);
-		$tv = imagecreatefrompng("filter_layers/tv.png");
+		$base = $this->get_blank_base();
+		$tv = $this->load_image("filter_layers/tv.png");
 		$rotated  = imagerotate($this->photo, 3, 0);
 		
 		imagefilter($rotated, IMG_FILTER_GRAYSCALE);
@@ -72,14 +59,96 @@ class filter
 	
 	private function mirror()
 	{
-		$base = imagecreatetruecolor($this::WIDTH, $this::HEIGHT);
-		$mirror = imagecreatefrompng("filter_layers/mirror.png");
+		$base = $this->get_blank_base();
+		$mirror = $this->load_image("filter_layers/mirror.png");
 		
 		imagefilter($this->photo, IMG_FILTER_BRIGHTNESS, -100);
 		imagecopyresampled($base, $this->photo, $this::WIDTH * 0.1 , $this::HEIGHT * 0.2, 0, 0, $this::WIDTH / 1.5, $this::HEIGHT / 1.5, $this::WIDTH, $this::HEIGHT);
 		imagecopyresampled($base, $mirror, 0, 0, 0, 0, $this::WIDTH, $this::HEIGHT, $this::WIDTH, $this::HEIGHT);
 		
 		return $base;
+	}
+	
+	private function oil()
+	{
+		$strength = 3;
+		$diff = 10;
+		$brushsize = 7;
+		$brushdiff = 3;
+	
+		for ($x = 0; $x < $this::WIDTH; $x += $strength)
+		{
+			for ($y = 0; $y < $this::HEIGHT; $y += $strength)
+			{
+				if(rand(0, $strength) < 2)
+				{
+					$rgb = imagecolorat($this->photo, $x, $y);
+					$modifier = $this->get_rand($diff);
+
+					$red = (($rgb >> 16) & 0xFF) + $modifier;
+					$green = (($rgb >> 8) & 0xFF) + $modifier;
+					$blue = ($rgb & 0xFF) + $modifier;
+
+					$red = $this->constrain_colour_value($red);
+					$green = $this->constrain_colour_value($green);
+					$blue = $this->constrain_colour_value($blue);
+					
+					$brushdiffx = $this->get_rand($brushdiff);
+					$brushdiffy = $this->get_rand($brushdiff);
+					
+					$colour = imagecolorallocate($this->photo, $red, $green, $blue);
+					imagefilledellipse($this->photo, $x + $brushdiffx, $y + $brushdiffy, $brushsize, $brushsize, $colour);
+				}
+			}
+		}
+		
+		return $this->photo;
+	}
+	
+	private function load_image($filename)
+	{
+		if(file_exists($filename))
+		{
+			$extension = substr($filename, -3);
+
+			switch($extension)
+			{
+				case 'png':
+					$image = imagecreatefrompng($filename);
+					break;
+				case 'gif':
+					$image = imagecreatefromgif($filename);
+					break;
+				default:
+					$image = imagecreatefromjpeg($filename);
+					break;
+			}
+
+			return $image;
+		}
+		else
+			die("file $filename doesn't exist");
+	}
+	
+	private function get_rand($range)
+	{
+		return rand($range * -1, $range);
+	}
+	
+	private function get_blank_base()
+	{
+		return imagecreatetruecolor($this::WIDTH, $this::HEIGHT);
+	}
+	
+	private function constrain_colour_value($colour, $low = 0, $high = 255)
+	{
+		if($colour < $low)
+			$colour = $low;
+
+		if($colour > $high)
+			$colour = $high;
+
+		return $colour;
 	}
 }
 
